a25270f5e648b8b09b540833463da0fbfa97b8ca
Aditi <saddi.5131@gmail.com>
2024-11-24 00:23:38 +0530

    fixed merges

--- 28-word-dictionary/main.go
+++ 28-word-dictionary/main.go
@@ -1,8 +1,8 @@
+
 package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
@@ -12,67 +12,62 @@ import (
 )
 
 const (
-	csvFilePath = "dictionary.csv"
-	indexPath = "index.dat"
-	changeLogPath = "changelog.csv"
+	csvFilePath    = "dictionary.csv"
+	indexPath      = "index.dat"
+	changeLogPath  = "changelog.csv"
 	newCsvFilePath = "new_dictionary.csv"
 	newIndexPath   = "new_index.dat"
 )
 
-func logError(msg string, err error){
-	if err != nil{
+func logError(msg string, err error) {
+	if err != nil {
 		log.Fatal(msg, err)
-		return
 	}
 }
 
-func createNewIndex(){
+func createNewIndex() {
 	start := time.Now()
 
 	// create a new index file
 	dat, err := os.Create(indexPath)
 	logError("Error creating the new index file", err)
-	
 	defer dat.Close()
 
 	// open the dictionary file
 	file, err := os.Open(csvFilePath)
 	logError("Error opening the dictionary file", err)
-	
 	defer file.Close()
 
-	// we loop thorugh the dictionary file, to create the byte offset index
 	// initialize scanner
 	scanner := bufio.NewScanner(file)
 	var offset int64
 
-	for scanner.Scan(){
+	for scanner.Scan() {
 		line := scanner.Text()
-		words := strings.Split(line, ",") // split the line 
+		words := strings.Split(line, ",") // split the line
 		_, err := dat.WriteString(fmt.Sprintf("%s %d\n", words[0], offset)) // store word space index
 		logError("Error writing to the index file", err)
-		
-		offset += int64(len(line)) + int64(len("\r\n")) // include newline chrct in offset in WINDOWS
+
+		offset += int64(len(line)) + int64(len("\r\n")) // include newline characters in offset
 	}
 	fmt.Printf("Created new index file successfully in %s\n\n", time.Since(start))
 }
 
-func wordLookup(word string) string{
+func wordLookup(word string) string {
 	start := time.Now()
 	// open and read the dat file - to search for word index
 	dat, err := os.Open(indexPath)
-	logError("Error opening index file", err) 
-	
+	logError("Error opening index file", err)
 	defer dat.Close()
 
 	scanner := bufio.NewScanner(dat)
 	var index int64 = -1
 
-	for scanner.Scan(){
+	for scanner.Scan() {
 		line := scanner.Text()
 		scannedWord := strings.Split(line, " ")
 
-		if word == scannedWord[0]{
+		if word == scannedWord[0] {
 			index, _ = strconv.ParseInt(scannedWord[1], 10, 64)
 			fmt.Printf("Found word %s at byte offset %d\n", scannedWord[0], index)
 			break
@@ -81,30 +76,32 @@ func wordLookup(word string) string{
 
 	// if word not found
 	if index == -1 {
-		fmt.Printf("Word %s not fond in dictionary", word)
+		fmt.Printf("Word %s not found in dictionary\n", word)
 		return ""
 	}
 
 	// open the csv file and directly read from the byte offset
 	file, err := os.Open(csvFilePath)
-	logError("Error opening the index file", err)
-	
+	logError("Error opening the dictionary file", err)
 	defer file.Close()
-	
+
 	// read line from the offset
 	file.Seek(index, 0) // Seek to the offset
 	reader := bufio.NewReader(file)
-	line, _ := reader.ReadString('\r') // WINDOWS \r
+	line, _ := reader.ReadString('\n') // read until newline
 	content := strings.Split(line, ",")
 
 	// word, meaning
+	if len(content) < 2 {
+		fmt.Printf("Error: Malformed line at offset %d\n", index)
+		return ""
+	}
 	fmt.Printf("%s : %s\nTime taken: %s\n\n", content[0], content[1], time.Since(start))
 
 	return content[1]
 }
 
-func merge(){
-
+func merge() {
 	// Open dictionary
 	dict, err := os.Open(csvFilePath)
 	logError("Error opening dictionary", err)
@@ -139,7 +136,7 @@ func merge(){
 
 	idxScanner.Scan()
 	chglogScanner.Scan()
-	
+
 	chglogParts := strings.Split(chglogScanner.Text(), ",")
 	chglogWord = chglogParts[0]
 	chglogMeaning = chglogParts[1]
@@ -147,35 +144,35 @@ func merge(){
 	dictWord = dictParts[0]
 	dictOffset, _ = strconv.ParseInt(dictParts[1], 10, 64)
 
-	// user pointers to merge
+	// use pointers to merge
 	for dictWord != "" || chglogWord != "" {
-		if chglogWord == "" || (dictWord != "" && strings.Compare(dictWord, chglogWord) < 0){
+		if chglogWord == "" || (dictWord != "" && strings.Compare(dictWord, chglogWord) < 0) {
 			dict.Seek(dictOffset, 0)
-			dictLine, _ := bufio.NewReader(dict).ReadString('\r')
+			dictLine, _ := bufio.NewReader(dict).ReadString('\n')
 
-			newDict.WriteString(dictLine + "\n")
+			newDict.WriteString(dictLine)
 			newIndex.WriteString(fmt.Sprintf("%s %d\n", dictWord, offset))
-			offset += int64(len(dictLine)) + int64(len("\r\n"))
+			offset += int64(len(dictLine))
 
 			// move dictoffset pointer
-			if idxScanner.Scan(){
+			if idxScanner.Scan() {
 				dictParts := strings.Split(idxScanner.Text(), " ")
 				dictWord = dictParts[0]
 				dictOffset, _ = strconv.ParseInt(dictParts[1], 10, 64)
-			} else{
+			} else {
 				dictWord = ""
 			}
-		}else if dictWord == "" || strings.Compare(chglogWord, dictWord) <= 0{ // = in case the word exist in both
+		} else if dictWord == "" || strings.Compare(chglogWord, dictWord) <= 0 { // = in case the word exists in both
 			newDict.WriteString(fmt.Sprintf("%s,%s\n", chglogWord, chglogMeaning))
 			newIndex.WriteString(fmt.Sprintf("%s %d\n", chglogWord, offset))
-			offset += int64(len(chglogWord) + len(chglogMeaning) + 2) + int64(len("\r\n"))
-			
+			offset += int64(len(chglogWord) + len(chglogMeaning) + 1) + int64(len("\n"))
+
 			// move changelog pointer
-			if chglogScanner.Scan(){
+			if chglogScanner.Scan() {
 				chglogParts := strings.Split(chglogScanner.Text(), ",")
 				chglogWord = chglogParts[0]
 				chglogMeaning = chglogParts[1]
-			} else{
+			} else {
 				chglogWord = ""
 			}
 		}
@@ -184,21 +181,19 @@ func merge(){
 	fmt.Println("Merge completed: New dictionary and index created.")
 }
 
-func addWordToChangelog(word, meaning string){
-	file, err := os.OpenFile(changeLogPath,  os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // creates when file not present
+func addWordToChangelog(word, meaning string) {
+	file, err := os.OpenFile(changeLogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) // creates when file not present
 	logError("Error creating the changelog file", err)
-	
 	defer file.Close()
 
 	// append word to the changelog
 	_, err = file.WriteString(fmt.Sprintf("%s,%s\n", word, meaning))
 	logError("Error appending to the changelog", err)
-	
-	fmt.Sprintf("Word added to changelog: %s\n", word)
+
+	fmt.Printf("Word added to changelog: %s\n", word)
 }
 
-func addWord(word, meaning string){
-	
+func addWord(word, meaning string) {
 	addWordToChangelog(word, meaning)
 	merge() // merge dictionary and changelog
 
@@ -207,14 +202,17 @@ func addWord(word, meaning string){
 	os.Remove(changeLogPath)
 }
 
-func main(){
+func main() {
 	createNewIndex()
 	wordLookup("Apple")
 	wordLookup("Dog")
 
-	addWord("Banana", "Yellow fruit, often eaten by monkeys and humans alike")
+	addWord("Banana", "Yellow fruit often eaten by monkeys and humans alike")
 	addWord("Elephant", "Large mammal with a trunk and grey colored")
-	addWord("Funnny", "A trait many people lack.")
+	addWord("Funny", "A trait many people lack.")
+	addWord("Dog", "Bow bow bow wow wow bark")
 
 	wordLookup("Elephant")
+	wordLookup("Orange")
+	wordLookup("Dog")
 }
